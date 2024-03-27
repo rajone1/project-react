4eb00ddc3c7c1948223727ed97f3fd5214d6e70b
rajone1 <rajoneweab101@gmail.com>
2024-03-27 21:31:44 +0600

    project2

--- practise/mypractise/src/App.jsx
+++ practise/mypractise/src/App.jsx
@@ -3,9 +3,30 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div></div>
-      <h1 className="text-4xl">practise</h1>
+      <div>
+        <h1 className="text-red-400 text-4xl">practise</h1>
+        <Name></Name>
+        <Name></Name>
+        <Name></Name>
+      </div>
     </>
+  );
+}
+
+function Name() {
+  let name = "shaon";
+  let information = {
+    age: 32,
+    address: "rajshahi",
+  };
+
+  return (
+    <div>
+      <h1>my name is {name}</h1>
+      <h2>
+        my age is {information.age} and i live in {information.address}
+      </h2>
+    </div>
   );
 }
 
